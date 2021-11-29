e4306614942bff4a9626eaf71b0602a342d9d493
Mark Vergunst <mark@symbiotic.nl>
2021-11-29 19:03:07 +0100

    new settings

--- project/settings.py
+++ project/settings.py
@@ -144,15 +144,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = "./var/data/statics/"
+# STATIC_URL = '/static/'
+# STATIC_ROOT = "./var/data/statics/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+#
 import os
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATIC_DIR = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
+#
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
