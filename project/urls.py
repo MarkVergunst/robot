b556d4038731af18e33ed1244b7f4a558cbf1a73
Mark Vergunst <mark@symbiotic.nl>
2021-11-29 20:20:34 +0100

    static files

--- project/urls.py
+++ project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -25,5 +26,10 @@ urlpatterns = [
     path('', index),
     path('play/<room_code>', game),
     path('showimg/', show_img),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
+
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
